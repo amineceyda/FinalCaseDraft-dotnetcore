ac79f7de43711ee60d2467bdd0334bc2c378fd04
amineceyda <amineceyda33@gmail.com>
2023-08-06 16:21:01 +0300

    error

--- SiteManangmentAPI/SiteManangmentAPI.Data/Repository/ContactRepository/IContactRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using SiteManangmentAPI.Data.Entities;
-using SiteManangmentAPI.Data.Repository.BaseRepository;
-
-namespace SiteManangmentAPI.Data.Repository.ContactRepository;
-
-public interface IContactRepository : IGenericRepository<Contact>
-{
-}
--- SiteManangmentAPI/SiteManangmentAPI.Data/Repository/MessageRepository/IMessageRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using SiteManangmentAPI.Data.Entities;
-using SiteManangmentAPI.Data.Repository.BaseRepository;
-
-namespace SiteManangmentAPI.Data.Repository.MessageRepository;
-
-public interface IMessageRepository : IGenericRepository<Message>
-{
-}
